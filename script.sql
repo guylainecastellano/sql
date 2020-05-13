a29def106c87fe488656c1072809244eb4914232
guylainecastellano <guylaine.a@wanadoo.fr>
2020-05-13 12:02:51 +0200

    php

--- script.sql
+++ script.sql
@@ -7,7 +7,7 @@ CREATE DATABASE IF NOT EXISTS `webdevelopment` charset='utf8';
 --PARTIE 1 ex5
 DROP DATABASE `langagues` ou si existe pas DROP DATABASE IF EXISTS `langagues`
 --PARTIE 2 ex2
-INSERT INTO webdevelopment (languageName, version)
+INSERT INTO webdevelopment (`languageName`, `version`)
 VALUES ('Javascript', 'Version 5'),
 ('PHP', 'Version 5.2'),
 ('PHP', 'Version 5.4'),
@@ -17,3 +17,21 @@ VALUES ('Javascript', 'Version 5'),
 ('Javascript', 'Version 7'),
 ('Javascript', 'Version 8'),
 ('PHP', 'Version 7');
+Afficher les données de la table par ordre alphabétique :
+USE webdevelopment
+SELECT `id`,`languageName`, `version`
+FROM `webdevelopment`
+ORDER BY `languageName` ;			ASC ordre ascendant ou DESC descendant
+Exécuter
+
+Afficher la table avec que les PHP  :
+USE `webdevelopment`
+SELECT `id`, `languageName`,
+FROM `webdevelopment`
+WHERE `languageName` ='PHP' ;
+
+Afficher toutes les versions PHP et Javascript :
+USE `webdevelopment`
+SELECT `id`, `languageName`,
+FROM `webdevelopment`
+WHERE `languageName` = 'PHP`  OR `languageName` =`Javascript` ;
